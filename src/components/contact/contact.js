2dace89c359505ebe80410fda7706a60f2217ddf
iser-80 <issamerrafiy01@gmail.com>
2023-11-02 17:02:59 +0100

    add emailjs to the contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import {FaPhoneAlt, FaFacebookF, FaTwitter, FaYoutube, FaInstagram, FaTelegramPlane} from 'react-icons/fa'
 import {FaEnvelope} from 'react-icons/fa6';
+import emailjs from '@emailjs/browser';
 import './contact.css'
+import { useState } from 'react';
+
 
 const Contact = () => {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      const form = document.getElementById('myForm');
+
+      emailjs.sendForm('service_lwa0fm2', 'template_f3rvm6f', form, 'r4fXDmuqn9H-UHN16')
+        .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+        console.log('FAILED...', error);
+        });
+    };
+  
+
   return (
     <div className='contactContainer'>
         <div className='contactWrapper'>
@@ -11,17 +28,17 @@ const Contact = () => {
             <div className='wrapper'>
                 <div className='contactForm'>
                     <h2>Send a Message</h2>
-                    <form>
+                    <form id='myForm' onSubmit={handleSubmit}>
                         <div className='fullName'>
-                            <input className='halfInput' type='text' placeholder='First Name'/>
+                            <input className='halfInput' name='user_name' type='text' placeholder='First Name'/>
                             <input className='halfInput' type='text' placeholder='Last Name'/>
                         </div>
                         <div className='phoneEmail'>
                             <input className='halfInput' type='number' placeholder='Phone'/>
-                            <input className='halfInput' type='email' placeholder='Email'/>
+                            <input className='halfInput' name='user_email' type='email' placeholder='Email'/>
                         </div>
                         <input className='fullInput' type='text' placeholder='Object'/>
-                        <textarea className='fullInput' rows={5} placeholder='Message'></textarea>
+                        <textarea className='fullInput' name='user_message' rows={5} placeholder='Message'></textarea>
                         <button className='contactBtn' type='submit'>Submit <FaTelegramPlane/></button>
                     </form>
                 </div>
